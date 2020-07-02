263358ba684886c3700b0582c4ce04b8f62c8cf4
Alexander Müller <aleksmueller@gmx.net>
2020-07-02 20:07:52 +0200

    * add argument to esm command

--- rplugin/python3/esm.py
+++ rplugin/python3/esm.py
@@ -24,9 +24,10 @@ class Esm(object):
                     return True
         return False
 
-
-    @pynvim.command('EsmUpdate', range='', nargs='*',sync=True)
-    def esm_up(self, args, range):
+    @pynvim.command('Esm', range='', nargs='*',sync=True)
+    def esm(self, args, range):
+        esm_option = args[0]
+        
         current_word = self.vim.eval('expand("<cword>")')
         current_file_path = self.vim.eval('expand("%:p")')
     
@@ -40,31 +41,33 @@ class Esm(object):
                 path = path + "\""
 
                 scratch_buffer = self.vim.api.create_buf(False, True)
-                win = self.esm_open_window(scratch_buffer)
-                self.esm_mappings(scratch_buffer, path)
+                win = self.esm_open_window(scratch_buffer, esm_option)
+                self.esm_mappings(scratch_buffer, path, esm_option)
 
-    # @pynvim.command('EsmCmd', range='', nargs='*',sync=True)
-    # def esmCmd(self, args, range):
-    #     scratch_buffer = self.vim.api.create_buf(False, True)
-    #     win = self.esm_open_window(scratch_buffer)
-    #     self.esm_mappings(scratch_buffer)
+    @pynvim.command('EsmUpdate', range='', nargs='*',sync=True)
+    def esm_up(self, args, range):
+
+    @pynvim.command('TestCommand', nargs='*', range='')
+    def testcommand(self, args, range):
+        self.vim.current.line = ('Command with args: {}, range: {}'
+                                  .format(args, range))
 
     @pynvim.function('EsmClose')
     def esm_close_win(self, args):
         self.vim.api.win_close(0, True)
 
 
-    def esm_open_window(self, buf):
-        opts = {'relative': 'cursor', 'width': 10, 'height': 2, 'col': 0, 'row': 1, 'anchor': 'NW', 'style': 'minimal'}
-        self.vim.api.buf_set_lines(buf, 0, -1, True, ["blub", "text"])
+    def esm_open_window(self, buf, command):
+        opts = {'relative': 'cursor', 'width': 10, 'height': 4, 'col': 0, 'row': 1, 'anchor': 'NW', 'style': 'minimal'}
+        self.vim.api.buf_set_lines(buf, 0, -1, True, [command, "=============", "blub", "text"])
         win = self.vim.api.open_win(buf, 1, opts)
 
 
-    def esm_mappings(self, buf, path):
+    def esm_mappings(self, buf, path, command):
         mappings = {
-                'e' : ':echo "element ' + path + '"<cr>',
-                'f' : ':echo "file ' + path + '"<cr>',
-                'g' : ':echo "group ' + path + '"<cr>',
+                'e' : ':echo " ' + command + ' element ' + path + '"<cr>',
+                'f' : ':echo " ' + command + ' file ' + path + '"<cr>',
+                'g' : ':echo " ' + command + ' group ' + path + '"<cr>',
                 'q' : ':exec EsmClose()<cr>',
                 '<cr>': ':exec EsmClose()<cr>'}
 
