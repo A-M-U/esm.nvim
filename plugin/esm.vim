9ba1d2fa0da58bdf28bb9027c73d01920cf34123
Alexander Mueller <blub@blub.de>
2020-07-03 15:32:45 +0200

    add default key mappings for vim normal mode

---
+++ plugin/esm.vim
@@ -0,0 +1,20 @@
+" svnext udpate element
+nnoremap <Leader>ee :EsmQuick update e<CR>
+" svnext udpate group
+nnoremap <Leader>eg :EsmQuick update g<CR>
+" svnext udpate file
+nnoremap <Leader>ef :EsmQuick update f<CR>
+
+" svnext diff element
+nnoremap <Leader>ede :EsmQuick diff e<CR>
+" svnext diff group
+nnoremap <Leader>edg :EsmQuick diff g<CR>
+" svnext diff file
+nnoremap <Leader>edf :EsmQuick diff f<CR>
+
+" svnext clean element
+nnoremap <Leader>ede :EsmQuick clean e<CR>
+" svnext clean group
+nnoremap <Leader>edg :EsmQuick clean g<CR>
+" svnext clean file
+nnoremap <Leader>edf :EsmQuick clean f<CR>
--- rplugin/python3/esm.py
+++ rplugin/python3/esm.py
@@ -9,13 +9,55 @@ class Esm(object):
     def __init__(self, vim):
         self.vim = vim
 
-    def esm_find_key(self, dictionary, keyword):
+    @pynvim.command('Esm', range='', nargs='*',sync=True)
+    def esm(self, args, range):
+        # add checks for arg parsing
+        esm_command = args[0]
+        esm_command_option = args[1]
+
+        path = self._esm_get_key_path_in_dict()
+        if path is not None:
+            scratch_buffer = self.vim.api.create_buf(False, True)
+            win = self._esm_open_window(scratch_buffer, esm_command)
+            self._esm_mappings(scratch_buffer, path, esm_command, esm_command_option)
+
+
+    @pynvim.function('EsmClose')
+    def esm_close_win(self, args):
+        self.vim.api.win_close(0, True)
+
+
+    @pynvim.command('EsmQuick', range='', nargs='*',sync=True)
+    def esm_quick(self, args, range):
+        # add checks for arg parsing
+        esm_command = args[0]
+        esm_command_option = args[1]
+
+        path = self._esm_get_key_path_in_dict()
+        if path is not None:
+            self.vim.command(':!svnext ' + esm_command + ' --overwrite-local-changes -' + esm_command_option + ' ' + path)
+
+
+    def _esm_get_key_path_in_dict(self):
+        current_word = (self.vim.eval('expand("<cWORD>")')).strip(':')
+        current_file_path = self.vim.eval('expand("%:p")')
+    
+        self.found_list.clear()
+        with open(current_file_path) as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+            
+            if self._esm_find_key_in_dict(data, current_word):
+                path = "/".join(self.found_list[::-1])
+        return path
+
+
+    def _esm_find_key_in_dict(self, dictionary, keyword):
         for k, v in dictionary.items():
             if k == keyword:
                 self.found_list.append(k)
                 return True
             if isinstance(v, dict):
-                if self.esm_find_key(v, keyword):
+                if self._esm_find_key_in_dict(v, keyword):
                     self.found_list.append(k)
                     return True
             else:
@@ -24,50 +66,20 @@ class Esm(object):
                     return True
         return False
 
-    @pynvim.command('Esm', range='', nargs='*',sync=True)
-    def esm(self, args, range):
-        esm_option = args[0]
-        
-        current_word = self.vim.eval('expand("<cword>")')
-        current_file_path = self.vim.eval('expand("%:p")')
-    
-        with open(current_file_path) as f:
-            data = yaml.load(f, Loader=yaml.FullLoader)
-            
-            if self.esm_find_key(data, current_word):
-                path = "\""
-                for i in self.found_list:
-                    path = path + str(i) + '/'
-                path = path + "\""
 
-                scratch_buffer = self.vim.api.create_buf(False, True)
-                win = self.esm_open_window(scratch_buffer, esm_option)
-                self.esm_mappings(scratch_buffer, path, esm_option)
-
-    @pynvim.command('EsmUpdate', range='', nargs='*',sync=True)
-    def esm_up(self, args, range):
-
-    @pynvim.command('TestCommand', nargs='*', range='')
-    def testcommand(self, args, range):
-        self.vim.current.line = ('Command with args: {}, range: {}'
-                                  .format(args, range))
-
-    @pynvim.function('EsmClose')
-    def esm_close_win(self, args):
-        self.vim.api.win_close(0, True)
-
-
-    def esm_open_window(self, buf, command):
-        opts = {'relative': 'cursor', 'width': 10, 'height': 4, 'col': 0, 'row': 1, 'anchor': 'NW', 'style': 'minimal'}
-        self.vim.api.buf_set_lines(buf, 0, -1, True, [command, "=============", "blub", "text"])
+    def _esm_open_window(self, buf, esm_command):
+        opts = {'relative': 'cursor', 'width': 20, 'height': 6, 'col': 0, 'row': 1, 'anchor': 'NW', 'style': 'minimal'}
+        self.vim.api.buf_set_lines(buf, 0, -1, True, [esm_command, "=====================", 
+                                                      "e: element", "g: group", "f: file", "q: quit"])
         win = self.vim.api.open_win(buf, 1, opts)
 
 
-    def esm_mappings(self, buf, path, command):
+    def _esm_mappings(self, buf, path, esm_command, esm_command_option):
+        # execute command under cursor if hitting enter
         mappings = {
-                'e' : ':echo " ' + command + ' element ' + path + '"<cr>',
-                'f' : ':echo " ' + command + ' file ' + path + '"<cr>',
-                'g' : ':echo " ' + command + ' group ' + path + '"<cr>',
+                'e' : ':exec EsmClose()<cr>:!svnext ' + esm_command + ' --overwrite-local-changes -' + esm_command_option + ' ' + path + '<cr>',
+                'g' : ':exec EsmClose()<cr>:!svnext ' + esm_command + ' --overwrite-local-changes -' + esm_command_option + ' ' + path + '<cr>',
+                'f' : ':exec EsmClose()<cr>:!svnext ' + esm_command + ' --overwrite-local-changes -' + esm_command_option + ' ' + path + '<cr>',
                 'q' : ':exec EsmClose()<cr>',
                 '<cr>': ':exec EsmClose()<cr>'}
 
